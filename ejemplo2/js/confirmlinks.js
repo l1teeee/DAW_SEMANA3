aa58292af27441dd46bcd4f64f7b0b061784c629
l1teeee <alejandro23.am29@gmail.com>
2022-07-25 21:32:06 -0600

    Actualización

--- ejemplo2/js/confirmlinks.js
+++ ejemplo2/js/confirmlinks.js
@@ -1,17 +1,17 @@
-//Iniciando el manejador para los enlaces de la página.
-window.onload = confirmAllLinks;
-//Controlador de eventos para elementos de enlace y area en mapas.
-//Utiliza la palabra clave this para hacer referencia al elemento
-//del documento y puede devolver false para evitar que
-//el navegador vaya al vínculo.
+//iniciando el manejdor para los enlaces de la pagina
+window.onload=confirmAlllinks;
+
+//controlador de eventos para elementos de enlace y area en mapas
+//utiliza la palabra clave this para hacer referencia al elemento
+//el navegador vaya al vinculo
+
 function confirmLink(){
-return confirm("¿Está seguro que quiere visitar el sitio: " + this.href
-+ "?");
-}
-//Recorrer todos los enlaces (hipervínculos) del documento y asignar
-//la función confirmLink a cada uno como controlador controlador de evento.
+    return confirm("¿Está seguro que quiere visitar el sitio: " + this.href
+    + "?");
+}//recorrer todos los enlaces hipervinculos del documento 
+//y asignar la funcion confirmLink a cada uno como controlador controlador de evento.
 function confirmAllLinks(){
-for(var i=0; i<document.links.length; i++){
-document.links[i].onclick = confirmLink;
-}
+    for(var i=0; i<document.links.length; i++){
+    document.links[i].onclick = confirmLink;
+    }
 }
--- ejemplo5/js/aleatorio.js
+++ ejemplo5/js/aleatorio.js
@@ -1,45 +1,42 @@
 var aleatorios;
-var min, max, cantidad;
-var disparador,listado;
+var min,max,cantidad;
 function init(){
-//Declaración del arreglo o matriz de forma literal
-//que contendrá los números aleatorios
-aleatorios = [];
-//Declaración y asignación de los límites entre
-//los cuales estarán comprendidos los números
-aleatorios
-min = parseInt(prompt("Ingrese el límite inferior",""));
-max = parseInt(prompt("Ingrese el límite superior",""));
-cantidad = parseInt(prompt("Indique la cantidad denúmeros aleatorios a generar", ""));
-disparador = document.getElementById("generador");
-listado = document.getElementById("listanumeros");
-if(disparador.addEventListener){
-disparador.addEventListener('click',
-function(evt){
-aleatorio(min, max, cantidad);
-listado.innerHTML = "Los números aleatorios son: " + aleatorios.toString();
-}, false);
-}
-else if(disparador.attachEvent){
-disparador.attachEvent('onclick', function(evt){
-aleatorio(min, max, cantidad);
-listado.innerHTML = "Los números aleatorios son: " + aleatorios.toString();
-});
-}
+    //declaracion del arreglo o matriz de forma literal
+    //que contendra los numeros aleatorios
+    aleatorios=[];
+    //declaracion y asignacion de los limites entre
+    //los cuales estaran comprendidos los numeros aleatorios
+    min=parseInt(prompt("Ingrese el limite inferior",""));
+    max=parseInt(prompt("Ingrese el limite superior",""));
+    cantidad=parseInt(prompt("Indique la cantidad de numeros aleatorios a generar",""));
+    disparador=document.getElementById("generador");
+    listado=document.getElementById("listanumeros");
+    if(disparador.addEventListener){
+        disparador.addEventListener('click',function(evt){
+            aleatorio(min, max, cantidad);
+            listado.innerHTML="Los numeros aleatorios son: "+aleatorios.toString();
+        }, false);
+        
+    }else if(disparador.attachEvent){
+        disparador.attachEvent('onclick', function(evt){
+            aleatorio(min, max,cantidad);
+            listado.innerHTML="los numeros aleatorios son: "+aleatorios.toString();
+
+        });
+    }
 }
 function aleatorio(minimo, maximo, cantidad){
-//Generando un número aleatorio teniendo el cuidado
-//que esté comprendido entre los valores mínimo y
-máximo
-var numero = Math.floor(Math.random() * (maximo -minimo + 1)) + minimo;
-//Verificar que no se haya llegado a la cantidad
-//de números aleatorios máximo establecidos
-if(aleatorios.length < cantidad){
-//Agregar el número aleatorio al arreglo o matriz
-//utilizando el método push()
-aleatorios.push(numero);
-//Invocar recursivamente a la función aleatorio
-aleatorio(minimo, maximo, cantidad);
+    //generando un numero aleatorio teniendo el cuidado
+    //que este comprendido entre los valores minimo y maximo
+    var numero=Math.floor(Math.random()*(maximo-minimo+1))+minimo;
+    //verificar que no se haya llegado a la cantidad
+    //de numero aleatorios maximo establecido
+    if (aleatorios.length<cantidad){
+        //agregar el numero aleatorio al arreglo o matiz
+        //utilizando el metodo push()
+        aleatorios.push(numero);
+        //invocar recursivamente a la funcion aleatorio
+        aleatorio(minimo, maximo, cantidad);
+    }
 }
-}
-window.onload = init;
+window.onload=init;
